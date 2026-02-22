c91a3fb729c3bb5e39bfa7c8990c9b6f0090b111
Echo <github@3kh0.net>
2026-02-22 08:36:51 -0800

    itch io builds

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  ssr: false,
+  nitro: {
+    output: { dir: 'dist' },
+    preset: 'static',
+  },
   css: [
     '@/assets/index.css',
   ],
